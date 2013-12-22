7db12578e636d9e180e5a8f59b7fbe8f9970c35c
Daniel Choi <dhchoi@gmail.com>
2013-12-22 12:44:05 -0500

    readerT monad exercise tweak

--- reader.hs
+++ reader.hs
@@ -6,8 +6,20 @@ reader1 = do
     e <- ask
     return $ length e
 
+readerT :: ReaderT Int IO Int
+readerT = do
+    e <- ask
+    liftIO $ putStrLn $ "Enter a string, Will return the length + " ++ show e ++ " (in ReaderT): "
+    s <- liftIO $ getLine
+    return $ e + (length s)
+
 main = do 
     putStrLn "Enter a string, I'll return the length: "
     s <- getLine 
     print $ runReader reader1 s
+    putStrLn "Enter a number"
+    x <- fmap read getLine 
+    r <- runReaderT readerT x
+    print r
+
 
