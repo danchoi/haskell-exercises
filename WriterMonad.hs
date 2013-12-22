bc187eba2d705cc9438a77fbe1ea8ee981144eb3
Daniel Choi <dhchoi@gmail.com>
2013-12-22 12:30:38 -0500

    WriterT exercise

--- WriterMonad.hs
+++ WriterMonad.hs
@@ -7,5 +7,15 @@ writer1 = do
   tell ["hello"]
   return "world"
 
+writerT :: WriterT [String] IO String
+writerT = do
+  tell ["hello"]
+  liftIO $ putStrLn "Enter a string"
+  input <- liftIO $ getLine
+  return input
+
+
 main = do 
   print $ runWriter writer1
+  r <- runWriterT writerT
+  print r
