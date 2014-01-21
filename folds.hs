58a01535d2c1600c3d00f2a7ffd5506d7de8ac71
Daniel Choi <dhchoi@gmail.com>
2014-01-21 11:41:05 -0500

    map as foldr

--- folds.hs
+++ folds.hs
@@ -17,6 +17,11 @@ foldlSum xs = foldl step 0 xs
 
 foldlSum2 xs = foldl (+) 0 xs
 
+foldrMap :: (a -> b) -> [a] -> [b]
+foldrMap f as = foldr step [] as
+  where step a bs  = (f a) : bs
+  
+
 main = do
   let xs = [1,2,3]
 
@@ -25,5 +30,6 @@ main = do
   print (foldlSum xs == sum xs)
   print (foldlSum2 xs == sum xs)
 
+  print (foldrMap (*2) xs == map (*2) xs)
 
 
