01a6eac6acfbde11cac3747b14a06991ef3ae3cf
Daniel Choi <dhchoi@gmail.com>
2014-01-21 11:36:36 -0500

    foldl exercise

--- folds.hs
+++ folds.hs
@@ -5,16 +5,25 @@ module Main where
 
 -- filter implemented with foldr
 
-foldrfilter :: (a -> Bool) -> [a] -> [a]
-foldrfilter p xs = foldr step [] xs
+foldrFilter :: (a -> Bool) -> [a] -> [a]
+foldrFilter p xs = foldr step [] xs
   where step x ys | p x       = x:ys
                   | otherwise = ys
 
+-- sum as a foldl
+foldlSum :: Num a => [a] -> a
+foldlSum xs = foldl step 0 xs
+  where step acc x = acc + x
+
+foldlSum2 xs = foldl (+) 0 xs
 
 main = do
   let xs = [1,2,3]
-  print (foldrfilter (>=2) xs == filter (>= 2) xs)
-                   
+
+  print (foldrFilter (>=2) xs == foldrFilter (>= 2) xs)
+  
+  print (foldlSum xs == sum xs)
+  print (foldlSum2 xs == sum xs)
 
 
 
