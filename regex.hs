17929327afba8e413f18238bad2d92c39a6954d1
Daniel Choi <dhchoi@gmail.com>
2014-01-24 14:47:02 -0500

    AllTextMatches type of result

--- regex.hs
+++ regex.hs
@@ -6,20 +6,27 @@ s1 = "To the last syllable of recorded time time"
 
 main = do
   print $ (s1 =~ "time" :: Bool)
-  print $ (s1 =~ "time" :: Int)
+  print $ (s1 =~ "time" :: Int) 
   print $ (s1 =~ "time" :: String)
   print $ (s1 =~ "time" :: [MatchArray])
   print $ (s1 =~ "time" :: [MatchText String])
-  let n :: AllTextMatches [] String
-      n = s1 =~ "time"
-  print $ getAllTextMatches n
+  print $ getAllTextMatches $ (s1 =~ "(time|recorded)" :: AllTextMatches [] String)
+  print $ getAllTextMatches $ (s1 =~ "time" :: AllTextMatches [] String)
 
 
 {-
+True
+2
+"time"
+[array (0,0) [(0,(33,4))],array (0,0) [(0,(38,4))]]
+[array (0,0) [(0,("time",(33,4)))],array (0,0) [(0,("time",(38,4)))]]
+["recorded","time","time"]
+["time","time"]
 
 see
 http://hackage.haskell.org/package/regex-base-0.93.2/docs/Text-Regex-Base-RegexLike.html#t:RegexContext
 
+http://lukeplant.me.uk/blog/posts/haskell-api-docs-suck-a-lot/
 
 -}
 
