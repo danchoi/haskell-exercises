bca97d1407cece9127cd7216d3c456c159df1c66
Daniel Choi <dhchoi@gmail.com>
2014-01-24 09:29:24 -0500

    IO works as a Monad here too

--- errors.hs
+++ errors.hs
@@ -15,8 +15,10 @@ main = do
   print $ ((50 `divBy` [5, 10, 25]) :: Maybe [Int])
   print $ ((50 `divBy` [5, 0, 25]) :: Maybe [Int])
   print $ ((50 `divBy` [5, 10, 25]) :: [[Integer]])
-  print $ ((50 `divBy` [5, 10, 25]) :: Either String [Integer])
+  ((50 `divBy` [5, 10, 25]) :: IO [Integer]) >>= print
+  print $ ((50 `divBy` [5, 10, 25]) :: Either String [Integer])  -- IO monad
   print $ ((50 `divBy` [5, 10, 25]) :: ((->) String [Int])) "test" -- reader monad
+  print $ ((50 `divBy` [5, 0, 25]) :: Either String [Integer])
 
 
 {-
@@ -26,6 +28,7 @@ Just [10,5,2]
 Just [10,5,2]
 Nothing
 [[10,5,2]]
+[10,5,2]
 Right [10,5,2]
 [10,5,2]
 
