30d0788aee6381345d5f7c736ef02d5d2736c342
Daniel Choi <dhchoi@gmail.com>
2014-01-28 15:45:06 -0500

    parse tag words and noise

--- attoparsec.hs
+++ attoparsec.hs
@@ -1,11 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Prelude hiding (takeWhile)
+import Data.Char
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Attoparsec.Text
+import Control.Applicative
+
+testString = "hello world one two three red banana mars"
 
 
-testString = "hello there"
+p1 :: Parser Text
 p1 = string "hello"
 
 
+data Chunk = Tag Text | Noise Text
+  deriving (Show)
+
+
+p2 :: Parser [Chunk]
+p2 = (choice $ tags ++ [noise]) `sepBy1` many1 space
+
+tags = map f [string "hello", string "world", string "hello world", string "one two", string "red"]
+    where f = (\parser -> Tag <$> parser )
+noise = Noise <$> takeWhile1 (not. isSpace)
+
 main = do
-  print $ parse p1 testString
+  parseTest p1 testString
+  let res = parseOnly p2 testString
+  print res
+  print $ [x | x@(Tag {}) <- either (const []) id res]
