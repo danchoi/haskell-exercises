b9ded7abc427100148a274e137a97ca380f2ca0a
Daniel Choi <dhchoi@gmail.com>
2014-01-30 13:13:03 -0500

    some Data.Time examples

--- exception.hs
+++ exception.hs
@@ -10,3 +10,8 @@ instance Exception MyException
 main = do
   throw MyException `catch` \e -> print (e :: MyException)
   throw (ErrorCall "ErrorCall: hello from error call") `catch` \e -> print (e :: SomeException)
+
+  -- note: Catching SomeException is not good practice
+
+  res <- try (readFile "nonexistent") :: IO (Either IOException String)
+  print res
---
+++ time.hs
@@ -0,0 +1,14 @@
+module Main where
+import Data.Time
+import Data.Time.Clock.POSIX
+
+main = do
+
+  -- Data.Time.Clock.POSIX utcTimeToPOSIXSeconds :: UTCTime -> POSIXTime
+  -- getCurrentTime   -- IO UTCTime
+  r <- utcTimeToPOSIXSeconds `fmap` getCurrentTime 
+  print r 
+  let r' = posixSecondsToUTCTime r
+  print r'
+  let x = 1364799600
+  print $ posixSecondsToUTCTime x
