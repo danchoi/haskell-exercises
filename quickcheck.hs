fd50df9c847f31d1f989c714fd540a12efb08f4e
Daniel Choi <dhchoi@gmail.com>
2014-02-07 14:11:02 -0500

    silly quickcheck example with Arbitrary instance

--- quickcheck.hs
+++ quickcheck.hs
@@ -7,16 +7,39 @@ prop_convert xs = xs == ((T.unpack . T.pack $ xs) :: String)
 
 data Color = Red | White | Blue deriving (Show, Ord, Eq, Read)
 
+newtype ColorString = ColorString String deriving (Show, Ord, Eq, Read)
+
 instance Arbitrary Color where
   arbitrary = do 
-    n <- choose (0,3) :: Gen Int
+    n <- choose (0,2) :: Gen Int
     return $ case n of 
       0 -> Red
       1 -> White
       2 -> Blue
 
+instance Arbitrary ColorString where
+  arbitrary = do 
+    n <- choose (0,2) :: Gen Int
+    return $ case n of 
+      0 -> ColorString "Red"
+      1 -> ColorString "White"
+      2 -> ColorString "Blue"
+      _ -> error "No colorstring to generate"
+
+colorToColorString = ColorString . show 
+
+stringToColor :: ColorString -> Color
+stringToColor (ColorString x) = read x
+stringToColor _  = error "Unknown Color"
+
+-- this is silly
+prop_color x = colorToColorString x == (colorToColorString . stringToColor . colorToColorString $ x)
+prop_colorstring x = stringToColor x == (stringToColor . colorToColorString . stringToColor $ x)
+
+
 main = do 
   quickCheck prop_convert
   quickCheckResult prop_convert
+  quickCheckResult prop_color
 
   -- verboseCheck prop_convert -- too noisy
