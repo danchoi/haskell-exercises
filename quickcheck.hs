0bb1bd7df4d50278c25faf51bf858787a0d1775d
Daniel Choi <dhchoi@gmail.com>
2014-02-07 14:01:19 -0500

    stub next example

--- quickcheck.hs
+++ quickcheck.hs
@@ -5,8 +5,18 @@ import qualified Data.Text as T
 
 prop_convert xs = xs == ((T.unpack . T.pack $ xs) :: String)
 
+data Color = Red | White | Blue deriving (Show, Ord, Eq, Read)
+
+instance Arbitrary Color where
+  arbitrary = do 
+    n <- choose (0,3) :: Gen Int
+    return $ case n of 
+      0 -> Red
+      1 -> White
+      2 -> Blue
 
 main = do 
   quickCheck prop_convert
   quickCheckResult prop_convert
-  verboseCheck prop_convert
+
+  -- verboseCheck prop_convert -- too noisy
