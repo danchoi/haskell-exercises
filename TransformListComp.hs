b054227b6236d7dc56a5740374f2d03264a50ee0
Daniel Choi <dhchoi@gmail.com>
2016-01-05 20:44:18 -0500

    refine with concat

--- TransformListComp.hs
+++ TransformListComp.hs
@@ -5,19 +5,19 @@ module Main where
 import GHC.Exts
 
 main = do
-    let xs = [("a", 1, ("c", "d"))
-             ,("a", 1, ("c1", "d1"))
-             ,("a", 0, ("c2", "d2"))
-             ,("b", 0, ("c3", "d3"))
+    let xs = [("a", 1, 3, ("c", "d"))
+             ,("a", 1, 3, ("c1", "d1"))
+             ,("a", 0, 5, ("c2", "d2"))
+             ,("b", 0, 6, ("c3", "d3"))
              ]
-    let xs' = [ (the a, the b, c) |
-                  x@(a,b,c) <- xs
+    let xs' = [ (the a, the b, c, d) |
+                  x@(a,b,c,d) <- xs
                 , then group by (a,b) using groupWith
                 ]
     mapM_ print xs'
     putStrLn "Second:"
-    let xs'' = [(the a, b, c) | (a,b,c)  <- xs', then group by a using groupWith]
 
+    let xs'' = [(the a, b, concat c, d) | (a,b,c, d)  <- xs', then group by a using groupWith]
     mapM_ print xs''
 
               
@@ -26,10 +26,11 @@ main = do
 -- groupWith :: Ord b => (a -> b) -> [a] -> [[a]]
 
 
--- ("a",1,["c","c1"],["d","d1"])
--- ("b",0,["c3"],["d3"])
--- ("b",1,["c4"],["dr"])
--- 
+{-
+-- runghc TransformListComp.hs
+-- ("a",0,[("c2","d2")])
+-- ("a",1,[("c","d"),("c1","d1")])
+-- ("b",0,[("c3","d3")])
 -- 
 
 
@@ -38,3 +39,11 @@ main = do
 -- ("b",[0],[[("c3","d3")]])
 -- 
 -- can zip these results together
+
+
+Second:
+("a",[0,1],[5,3,3],[[("c2","d2")],[("c","d"),("c1","d1")]])
+("b",[0],[6],[[("c3","d3")]])
+
+
+-}
