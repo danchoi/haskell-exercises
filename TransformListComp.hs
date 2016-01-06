d4cb8ce89f977689fed65964753914dd9bc960fd
Daniel Choi <dhchoi@gmail.com>
2016-01-05 19:31:16 -0500

    another variation

--- TransformListComp.hs
+++ TransformListComp.hs
@@ -5,15 +5,22 @@ module Main where
 import GHC.Exts
 
 main = do
-    let xs = [("a", 1, "c", "d")
-             ,("a", 1, "c1", "d1")
-             ,("b", 0, "c3", "d3")
-             ,("b", 1, "c4", "dr")]
-    let xs' = [ (the a, the b, c, d) |
-                  x@(a,b,c,d) <- xs
+    let xs = [("a", 1, ("c", "d"))
+             ,("a", 1, ("c1", "d1"))
+             ,("a", 0, ("c2", "d2"))
+             ,("b", 0, ("c3", "d3"))
+             ]
+    let xs' = [ (the a, the b, c) |
+                  x@(a,b,c) <- xs
                 , then group by (a,b) using groupWith
                 ]
     mapM_ print xs'
+    putStrLn "Second:"
+    let xs'' = [(the a, b, c) | (a,b,c)  <- xs', then group by a using groupWith]
+
+    mapM_ print xs''
+
+              
       
 
 -- groupWith :: Ord b => (a -> b) -> [a] -> [[a]]
@@ -24,3 +31,10 @@ main = do
 -- ("b",1,["c4"],["dr"])
 -- 
 -- 
+
+
+-- Second:
+-- ("a",[0,1],[[("c2","d2")],[("c","d"),("c1","d1")]])
+-- ("b",[0],[[("c3","d3")]])
+-- 
+-- can zip these results together
